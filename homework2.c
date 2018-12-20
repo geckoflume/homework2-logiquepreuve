33959a4b0cecb46fc17edf4ab6b58ca5f658ed96
geckoflume <Florian.Mornet@bordeaux-inp.fr>
2018-12-20 18:42:59 +0100

    Working version

--- homework2.c
+++ homework2.c
@@ -16,7 +16,7 @@ int palindrome_iter(const char *w, int n) {
 }
 
 /*
-int palindrome_iter2(const char *w, int n) {
+int palindrome_iter(const char *w, int n) {
   int i = 0;
 
   while (i < n && w[i] == w[n-i-1] && i < n-i-1) {
